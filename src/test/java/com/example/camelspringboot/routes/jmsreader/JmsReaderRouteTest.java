b939b1dfffb6795cf5f9a70420ff8aef88e7b887
Dennis <d.van.loon@portbase.com>
2026-02-04 16:50:09 +0100

    Fix build

--- src/test/java/com/example/camelspringboot/routes/jmsreader/JmsReaderRouteTest.java
+++ src/test/java/com/example/camelspringboot/routes/jmsreader/JmsReaderRouteTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @CamelSpringBootTest
 @SpringBootTest
 @MockEndpoints("activemq:*")
